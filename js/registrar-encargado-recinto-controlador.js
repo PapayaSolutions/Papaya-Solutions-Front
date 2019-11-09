655cb2fd06e199b46c687502bfd1c9ddba306ba7
Wilfredodiazalguera <wdiaza@ucenfotec.ac.cr>
2019-11-08 18:08:23 -0600

    Se realiza la validacion de la edad
    Se realiza la validacion de la edad
    
    funcion de validar edad

--- js/registrar-encargado-recinto-controlador.js
+++ js/registrar-encargado-recinto-controlador.js
@@ -52,17 +52,49 @@ const validar = () => {
 };
 
 // function para generar codigos
-var fecha1 = moment('2016-07-12');
-var fecha2 = moment('2016-08-01');
-
-console.log(fecha2.diff(fecha1, 'days'), ' dias de diferencia');
-
-// function para generar codigos
 function codigoVer(length, chars) {
     let result = '';
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
 }
+
+// function para validar cantidad de digitos en campo numeros
+const validar_numero = (numero) => {
+    let error_num = false
+    if (numero.length < 8) {
+        error_num = true
+    } else {
+        error_num = false
+    }
+    return error_num
+}
+
+// function para validar la mayoria de edad
+const validar_edad = (fecha) => {
+    let error_edad = false
+    let hoy = new Date();
+    let fecha_conv = new Date(fecha);
+    let dia_actual = hoy.getDate()
+    let dia_fecha = fecha_conv.getDate() + 1
+    let anos = (hoy.getFullYear() - fecha_conv.getFullYear())
+    let mes_actual = hoy.getMonth() + 1
+    let mes_fecha = fecha_conv.getMonth() + 1
+
+    console.log('Dia actual: ' + dia_actual)
+    console.log('Dia fecha: ' + dia_fecha)
+    console.log('Mes actual: ' + mes_actual)
+    console.log('Mes fecha: ' + mes_fecha)
+    console.log('Year : ' + anos)
+
+    if (dia_actual < dia_fecha || anos < 18 || mes_actual > mes_fecha) {
+        error_edad = true
+    } else {
+        error_edad = false
+    }
+    return error_edad
+
+}
+
 
 // function obtener_datos
 let obtener_datos = () => {
@@ -76,6 +108,9 @@ let obtener_datos = () => {
     let contrasena = codigoVer(20, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
     //  console.log(contrasena)
 
+
+
+
     //Si hay error entra al if     
     if (validar()) {
         Swal.fire({
@@ -83,6 +118,22 @@ let obtener_datos = () => {
             title: 'Faltan datos',
             text: 'Verifique los campos!',
         })
+
+    } else if (validar_numero(numero)) {
+        Swal.fire({
+            type: 'warning',
+            title: 'Verifique el campo numero',
+            text: 'Minimo debe de tener 8 digitos!',
+        })
+
+    } else if (validar_edad(fecha_nacimiento)) {
+        Swal.fire({
+            type: 'warning',
+            title: 'Verifique la edad',
+            text: 'Debe de ser mayor de edad!',
+        })
+
+
     } else {
         Swal.fire({
             type: 'success',

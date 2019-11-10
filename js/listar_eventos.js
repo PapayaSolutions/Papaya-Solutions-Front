d8a9bef66317381314e218e08549b54b0780beb1
Daniel <55204348+Daniel-Rodriguez1@users.noreply.github.com>
2019-11-09 23:43:45 -0600

    ya conecta con la DB

--- js/listar_eventos.js
+++ js/listar_eventos.js
@@ -1,30 +1,29 @@
 'use strict'
 const tbody = document.querySelector('#tbl-personas tbody');
 const input_filtro = document.querySelector('#txt-filtro');
-let lista_personas;
+let lista_evento;
 
 let llenar_tabla = async() => {
     let filtro = input_filtro.value.toLowerCase();
-    lista_personas = await listar_personas();
+    lista_evento = await listar_evento();
 
     tbody.innerHTML = '';
-    for (let i = 0; i < lista_personas.length; i++) {
-        let nombre = lista_personas[i]['nombre'].toLowerCase();
+    for (let i = 0; i < lista_evento.length; i++) {
+        let nombre = lista_evento[i]['nombre'].toLowerCase();
         if (nombre.includes(filtro)) {
 
             let fila = tbody.insertRow();
 
-            fila.insertCell().innerHTML = lista_personas[i]['cedula'];
-            fila.insertCell().innerHTML = lista_personas[i]['correo'];
-            fila.insertCell().innerHTML = lista_personas[i]['nombre'];
-            fila.insertCell().innerHTML = lista_personas[i]['apellido'];
-            fila.insertCell().innerHTML = lista_personas[i]['segundo_apellido'];
-            fila.insertCell().innerHTML = lista_personas[i]['estado'];
+            fila.insertCell().innerHTML = lista_evento[i]['id'];
+            fila.insertCell().innerHTML = lista_evento[i]['nombre'];
+            fila.insertCell().innerHTML = lista_evento[i]['tipo'];
+            fila.insertCell().innerHTML = lista_evento[i]['asistentes_esperados'];
+            fila.insertCell().innerHTML = lista_evento[i]['fecha_disponible'];
+            fila.insertCell().innerHTML = lista_evento[i]['pais_evento'];
         }
 
     };
 };
-
 
 llenar_tabla();
 input_filtro.addEventListener('keyup', llenar_tabla);

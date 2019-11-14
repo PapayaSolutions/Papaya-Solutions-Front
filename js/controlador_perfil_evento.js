0e8d204fbf9020f0a17080c58f4d33ce18ac0bee
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2019-11-13 18:55:44 -0600

    update total
    update a todos los files

--- js/controlador_perfil_evento.js
+++ js/controlador_perfil_evento.js
@@ -1,34 +1,50 @@
 'use strict';
 
-const nombre = document.querySelector('.nombre_evento');
-const recinto = document.querySelector('.recinto_evento');
-const fecha = document.querySelector('.fecha_evento');
-const hora = document.querySelector('.hora_evento');
-const direccion = document.querySelector('.direccion_evento');
-const precio = document.querySelector('.precio_evento');
-const categoria = document.querySelector('.categoria_evento');
-const descripcion = document.querySelector('.descripcion_evento');
-const estado;
-const organizador;
+const nombre = document.querySelector('#bnr_nombre');
+const recinto = document.querySelector('#recinto_evento');
+const fecha = document.querySelector('#fecha_evento');
+const hora = document.querySelector('#hora_evento');
+const direccion = document.querySelector('#direccion_evento');
+const precio = document.querySelector('#precio_evento');
+const categoria = document.querySelector('#categoria_evento');
+const descripcion = document.querySelector('#descripcion_evento');
+const cantidad = document.querySelector('#cantidad');
+
+
+let evento_id = localStorage.getItem('id_evento');
 
 let datos_evento;
 
 let llenar_perfil = async() => {
     datos_evento = await listar_evento(); /*en servicios del evento*/
+    /*extrae la info de la bd*/
+    let date = new Date(datos_evento[evento_id]['fecha_disponible']);
+    /* asigna las horas a tiempo */
+    let tiempo = date.getHours();
+    /**asigna pm o am */
+    if (tiempo > 12) {
+        tiempo = ((tiempo - 12) + 'pm').toString();
+    } else {
+        tiempo = (tiempo + 'am')
+    }
+    /**arreglo de dias de la semana */
+    var dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    var dayName = dias[date.getDay()];
+    /**arreglo de meses del año */
+    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var mesName = meses[date.getMonth()];
+
+    nombre.innerHTML = datos_evento[evento_id]['nombre'];
+    fecha.innerHTML = (dayName + '  ' + date.getDate() + ' de ' + mesName + ' del ' + date.getFullYear());
+    hora.value = tiempo;
+    recinto.value = datos_evento[evento_id]['recinto'];
+    precio.value = ('¢' + (datos_evento[evento_id]['precio_entrada']));
+    categoria.value = datos_evento[evento_id]['tipo'];
+    descripcion.value = datos_evento[evento_id]['descripcion'];
+    cantidad.value = datos_evento[evento_id]['cantidad_maxima_usuario'];
+    direccion.value = datos_evento[evento_id]['pais_evento'];
+
 
 }
 
 llenar_perfil();
-
-/*evento_id: { type: Number, required: true, unique: true },
-    nombre: { type: String, required: true, unique: false },
-    dia: { type: Date, required: true, unique: false },
-    hora: { type: Date, required: true, unique: false },
-    hora2: { type: Date, required: true, unique: false },
-    recinto: { type: String, required: true, unique: false },
-    direccion: { type: String, required: true, unique: false },
-    precio: { type: Number, required: true, unique: false },
-    categoria: { type: String, required: true, unique: false },
-    descripcion: { type: String, required: true, unique: false },
-    estado: { type: String, required: true, unique: false },
-    organizador: { type: String, required: true, unique: false }*/

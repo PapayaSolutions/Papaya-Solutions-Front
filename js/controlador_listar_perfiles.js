467a354d51453fa1e865b0292dd7351e382b8bf2
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2019-11-17 09:26:48 -0600

    cambios a tablas

--- js/controlador_listar_perfiles.js
+++ js/controlador_listar_perfiles.js
@@ -14,22 +14,15 @@ let llenar_tabla = async() => {
 
             let fila = tbody.insertRow();
 
-            /*  btn.onclick = (function(entry) { return function() { chooseUser(entry); } })(entry);*/
-
             fila.insertCell().innerHTML = lista_clientes[i]['p_nombre'];
             fila.insertCell().innerHTML = lista_clientes[i]['p_apellido'];
             fila.insertCell().innerHTML = lista_clientes[i]['s_apellido'];
             fila.insertCell().innerHTML = lista_clientes[i]['correo_cliente'];
+            fila.insertCell().innerHTML = lista_clientes[i]['edad'];
+            fila.insertCell().innerHTML = lista_clientes[i]['genero'];
+            fila.insertCell().innerHTML = lista_clientes[i]['direccion'];
+            fila.insertCell().innerHTML = lista_clientes[i]['tipo'];
             fila.insertCell().innerHTML = lista_clientes[i]['estado'];
-
-            let btn = document.createElement('input');
-            btn.type = "button";
-            btn.className = "btn";
-            btn.value = lista_clientes[i]['_id'];
-            /* td.appendChild(btn);*/
-            fila.insertCell().innerHTML = btn;
-
-            /* '<input type = "button">';*/
 
         }
     };

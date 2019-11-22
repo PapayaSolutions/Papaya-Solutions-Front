b517bf664c84cee367a660d91f7baee94447f668
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2019-11-22 09:37:17 -0600

    trabajo en perfil de evento
    css y fechas

--- js/controlador_perfil_evento.js
+++ js/controlador_perfil_evento.js
@@ -2,7 +2,7 @@
 
 const nombre = document.querySelector('#bnr_nombre');
 const recinto = document.querySelector('#recinto_evento');
-const fecha = document.querySelector('#fecha_evento');
+const tabla_fechas = document.querySelector('#fecha_evento tbody');
 const hora = document.querySelector('#hora_evento');
 
 const precio = document.querySelector('#precio_evento');
@@ -27,20 +27,74 @@ let llenar_perfil = async() => {
 
     imagen.src = datos_evento[0]['URL_imagen'];
 
+    for (let i = 0; i < datos_evento[0]['fecha_disponible'].length; i++) {
 
 
-    let date = new Date((datos_evento[0]['fecha_disponible'])[0]['fecha']);
+        let date = new Date((datos_evento[0]['fecha_disponible'])[i]['fecha']);
+        let tiempo = datos_evento[0]['fecha_disponible'][i]['hora'];
 
-    let tiempo = datos_evento[0]['fecha_disponible'][0]['hora'];
+        var dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        var dayName = dias[date.getDay()];
+        var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        var mesName = meses[date.getMonth()];
 
-    var dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-    var dayName = dias[date.getDay()];
-    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    var mesName = meses[date.getMonth()];
 
-    fecha.innerHTML = (dayName + '  ' + date.getDate() + ' de ' + mesName + ' del ' + date.getFullYear());
-    hora.value = tiempo;
+        let fila = tabla_fechas.insertRow();
+        fila.insertCell().innerHTML = (dayName + '  ' + date.getDate() + ' de ' + mesName + ' del ' + date.getFullYear());
 
+        fila.insertCell().innerHTML = tiempo;
+
+    }
 
 };
 llenar_perfil();
+
+var map;
+var marker;
+
+function initMap() {
+
+    var myLatLng = {
+        lat: 9.9323102,
+        lng: -84.0311761
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: myLatLng
+    });
+
+    map.addListener('click', function(e) {
+        placeMarkerAndPanTo(e.latLng, map);
+    });
+
+
+}
+
+
+function placeMarkerAndPanTo(latLng, map) {
+
+    if (marker != undefined) {
+        marker.position = latLng;
+    } else {
+        marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            draggable: true,
+        });
+    }
+    actualizar_ubicacion(latLng);
+
+    google.maps.event.addListener(marker, 'drag', function() {
+        actualizar_ubicacion(marker.position);
+    });
+
+    map.panTo(latLng);
+}
+
+const actualizar_ubicacion = position => {
+    let txt_latitud = document.querySelector('#txt_latitud');
+    let txt_longitud = document.querySelector('#txt_longitud');
+    txt_latitud.value = position.lat();
+    txt_longitud.value = position.lng();
+};

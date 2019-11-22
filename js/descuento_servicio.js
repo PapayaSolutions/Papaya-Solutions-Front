01041d3c931e16130200151a8eb72bdba4890fe7
Lycos11 <marcospc094@gmail.com>
2019-11-21 22:22:29 -0600

    Ccs,controller, server

---
+++ js/descuento_servicio.js
@@ -0,0 +1,36 @@
+let registrar_descuento = async(nombre, descipcion, porcentaje) => {
+    await axios({
+            method: 'post',
+            url: 'http://localhost:3000/api/descuento',
+            responseType: 'json',
+            //body
+            data: {
+                nombre: nombre,
+                descipcion: descipcion,
+                porcentaje: porcentaje,
+            }
+        })
+        .then(function(res) {
+            console.log(res.data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+};
+
+let listar_descuento = async() => {
+    let lista_descuento;
+    await axios({
+            method: 'get',
+            url: 'http://localhost:3000/api/listar_descuento',
+            responseType: 'json'
+        })
+        .then(function(res) {
+            lista_descuento = res.data.descuentos;
+
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    return lista_descuento;
+}

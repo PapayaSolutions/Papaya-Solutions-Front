cad709eabb18daf1c110b28bdcc5efff2f3adea4
Lycos11 <marcospc094@gmail.com>
2019-11-24 14:49:41 -0600

    visualizar

--- js/controlador_perfil_cliente.js
+++ js/controlador_perfil_cliente.js
@@ -1,4 +1,78 @@
 'use strict';
+
+const nombre = document.querySelector('#nombre');
+const genero = document.querySelector('#genero');
+const nacimiento = document.querySelector('#nacimiento');
+const tbody = document.querySelector('#tbl_perfil tbody');
+const direccion = document.querySelector('#direccion');
+const identificacion = document.querySelector('#identificacion');
+const correo_cliente = document.querySelector('#correo');
+
+let lista_clientes;
+let id = localStorage.getItem('id_perfil', tbody);
+
+let llenar_tabla = async() => {
+
+    lista_clientes = await listar_clientes(id);
+    console.log(lista_clientes);
+
+    let i = 3;
+    let fila = tbody.insertRow();
+
+    nombre.innerHTML = (lista_clientes[i]['p_nombre'] + ' ' + lista_clientes[i]['s_nombre'] + ' ' + lista_clientes[i]['p_apellido'] + ' ' + lista_clientes[i]['s_apellido']);
+    genero.innerHTML = lista_clientes[i]['genero'];
+
+    fila.insertCell().innerHTML = ('Dirección 2:' + '              ' + lista_clientes[i]['provincia'] + ' ' + lista_clientes[i]['canton'] + ' ' + lista_clientes[i]['distrito'] + ' ');
+    direccion.innerHTML = lista_clientes[i]['direccion'];
+    identificacion.innerHTML = lista_clientes[i]['identificacion'];
+    correo_cliente.innerHTML = lista_clientes[i]['correo_cliente'];
+
+    const calcular_edad = (f_nacimiento) => {
+
+        let error_edad = false;
+        let hoy = new Date();
+        let cumpleanos = new Date(f_nacimiento);
+        let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+        let m = hoy.getMonth() - cumpleanos.getMonth();
+
+        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate() + 1)) {
+            edad--;
+
+            if (edad < 18) {
+                error_edad = true
+
+            } else {
+                error_edad = false
+            }
+        }
+        return error_edad;
+
+    }
+    calcular_edad();
+    nacimiento.innerHTML = lista_clientes[i]['f_nacimiento'];
+
+    let perfil = fila.insertCell();
+
+    let boton = document.createElement('button');
+    boton.dataset.destino = lista_clientes[i]['_id'];
+    boton.innerText = 'Editar perfil';
+    boton.classList.add('btn');
+
+    boton.addEventListener('click', function() {
+        localStorage.setItem("destino_id", this.dataset.destino);
+        window.location.href = 'visualizar_perfil.html';
+
+    });
+
+    perfil.appendChild(boton);
+
+};
+
+llenar_tabla();
+
+
+
+/*
 const nombre = document.querySelector('nombre');
 const tbody = document.querySelector('#tbl_perfil tbody');
 
@@ -19,4 +93,4 @@ let llenar_perfil = async() => {
 
 };
 
-llenar_perfil();
+llenar_perfil();*/

bf46168a29b7667e7fb4c767ada30c1b9202583c
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2019-11-24 12:32:44 -0600

    cloudinary cliente

--- js/clientes_servicio.js
+++ js/clientes_servicio.js
@@ -71,3 +71,32 @@ let listar_clientes = async() => {
         });
     return lista_clientes;
 }
+
+//--------------------------- CLOUDINARY WIDGET --------------------------------------------------------
+var myWidget1 = cloudinary.createUploadWidget({
+    cloudName: 'pypsolutionscr',
+    uploadPreset: 'psolutions'
+}, (error, result) => {
+    if (!error && result && result.event === "success") {
+        console.log('Done! Here is the image info: ', result.info);
+        document.querySelector('#imagen_preview').src = result.info.secure_url;
+        registrar_avatar(result.info.original_filename, result.info.secure_url, 'Activo');
+
+        Swal.fire({
+            type: 'success',
+            title: 'Registro realizado con exito',
+            text: 'El Avatar ha sido almacenado',
+            confirmButtonText: 'Entendido'
+        }).then(function() {
+            location.reload();
+        });
+
+    }
+
+});
+
+let botn = document.querySelector('#btn_agregar_imagen');
+
+botn.addEventListener('click', function() {
+    myWidget1.open();
+}, false);

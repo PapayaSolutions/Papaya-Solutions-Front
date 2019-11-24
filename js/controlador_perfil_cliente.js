876a5c79a14cb04bdc45bac40e66a49707dd0c71
Lycos11 <marcospc094@gmail.com>
2019-11-23 23:08:01 -0600

    Update controlador_perfil_cliente.js

--- js/controlador_perfil_cliente.js
+++ js/controlador_perfil_cliente.js
@@ -1,23 +1,19 @@
 'use strict';
 
 const nombre = document.querySelector('#nombre_perfil');
-const correo_cliente = document.querySelector('#correo_perfil');
-const genero = document.querySelector('#genero_perfil');
 
-let lista_clientes;
+let lista_perfil;
 let id = localStorage.getItem('id_cliente');
 
 let llenar_perfil = async() => {
 
-    lista_clientes = await obtener_cliente_id(id);
-    for (let i = 0; i < lista_clientes.length; i++) {
-        nombre.innerHTML = lista_clientes[i]['p_nombre'];
-        correo_cliente.value = lista_clientes[i]['correo_cliente'];
-        genero.value = lista_clientes[i]['genero'];
+    lista_perfil = await obtener_cliente_id(id);
 
+    nombre.innerHTML = lista_perfil[0]['nombre'];
 
-    }
+    localStorage.setItem('nombre', nombre.value);
 
-};
-
+}
 llenar_perfil();
+
+localStorage.getItem(llenar_perfil);

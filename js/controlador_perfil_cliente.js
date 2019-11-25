726f2877695e74b584aed17214a5c1ff24fb91c0
Lycos11 <marcospc094@gmail.com>
2019-11-24 23:11:17 -0600

    Update controlador_perfil_cliente.js

--- js/controlador_perfil_cliente.js
+++ js/controlador_perfil_cliente.js
@@ -9,62 +9,35 @@ const identificacion = document.querySelector('#identificacion');
 const correo_cliente = document.querySelector('#correo');
 
 let lista_clientes;
-let id = localStorage.getItem('id_perfil', tbody);
+let id = localStorage.getItem('id_cliente');
 
 let llenar_tabla = async() => {
 
     lista_clientes = await listar_clientes(id);
     console.log(lista_clientes);
 
-    let i = 3;
     let fila = tbody.insertRow();
 
-    nombre.innerHTML = (lista_clientes[i]['p_nombre'] + ' ' + lista_clientes[i]['s_nombre'] + ' ' + lista_clientes[i]['p_apellido'] + ' ' + lista_clientes[i]['s_apellido']);
-    genero.innerHTML = lista_clientes[i]['genero'];
-
-    fila.insertCell().innerHTML = ('Dirección 2:' + '              ' + lista_clientes[i]['provincia'] + ' ' + lista_clientes[i]['canton'] + ' ' + lista_clientes[i]['distrito'] + ' ');
-    direccion.innerHTML = lista_clientes[i]['direccion'];
-    identificacion.innerHTML = lista_clientes[i]['identificacion'];
-    correo_cliente.innerHTML = lista_clientes[i]['correo_cliente'];
-
-    const calcular_edad = (f_nacimiento) => {
-
-        let error_edad = false;
-        let hoy = new Date();
-        let cumpleanos = new Date(f_nacimiento);
-        let edad = hoy.getFullYear() - cumpleanos.getFullYear();
-        let m = hoy.getMonth() - cumpleanos.getMonth();
-
-        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate() + 1)) {
-            edad--;
-
-            if (edad < 18) {
-                error_edad = true
-
-            } else {
-                error_edad = false
-            }
-        }
-        return error_edad;
-
-    }
-    calcular_edad();
-    nacimiento.innerHTML = lista_clientes[i]['f_nacimiento'];
+    nombre.innerHTML = (lista_clientes[0]['p_nombre'] + ' ' + lista_clientes[0]['s_nombre'] + ' ' + lista_clientes[0]['p_apellido'] + ' ' + lista_clientes[0]['s_apellido']);
+    genero.innerHTML = lista_clientes[0]['genero'];
+    fila.insertCell().innerHTML = ('Dirección 2:' + ' ' + lista_clientes[0]['provincia'] + ' ' + lista_clientes[0]['canton'] + ' ' + lista_clientes[0]['distrito'] + ' ');
+    direccion.innerHTML = lista_clientes[0]['direccion'];
+    identificacion.innerHTML = lista_clientes[0]['identificacion'];
+    correo_cliente.innerHTML = lista_clientes[0]['correo_cliente'];
+    nacimiento.innerHTML = lista_clientes[0]['f_nacimiento'];
 
     let perfil = fila.insertCell();
-
     let boton = document.createElement('button');
-    boton.dataset.destino = lista_clientes[i]['_id'];
     boton.innerText = 'Editar perfil';
     boton.classList.add('btn');
-
     boton.addEventListener('click', function() {
-        localStorage.setItem("destino_id", this.dataset.destino);
         window.location.href = 'visualizar_perfil.html';
-
     });
-
     perfil.appendChild(boton);
+
+    localStorage.setItem('id_cliente', JSON.stringify(lista_clientes));
+
+    JSON.parse(localStorage.getItem('id_cliente'));
 
 };
 

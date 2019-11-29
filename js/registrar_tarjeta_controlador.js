d02d861484bfa7b07dca2958e447198df98bfb6d
Lycos11 <marcospc094@gmail.com>
2019-11-28 22:48:59 -0600

    registrar tarjeta jiji

--- js/registrar_tarjeta_controlador.js
+++ js/registrar_tarjeta_controlador.js
@@ -8,6 +8,82 @@ const input_apellido = document.querySelector('#apellido');
 const input_postal = document.querySelector('#postal');
 const btn_registro = document.querySelector('#btn_registro');
 
+let tarjeta_valida = true;
+
+$("#tarjeta").keyup(function(e) {
+    var num = $(this).val().toString();
+    var charCount = num.length;
+
+    /* VALIDACION DE TIPO */
+    if (charCount == 1) {
+        if (num == "4") {
+            $("#type").html("VISA");
+        }
+    }
+    if (charCount == 2) {
+        if (num == "51" || num == "55" || num == "53") {
+            $("#type").html("MASTER CARD");
+        }
+    }
+
+    /* !VALIDACION DE TIPO */
+
+    /* ALGORITMO */
+    if (charCount == 13 || charCount == 14 || charCount == 15 || charCount == 16) {
+        var valid = isValid(num, charCount);
+        if (valid) {
+            $("#type").html("Valida");
+            $("input").attr("name", "numero_tarjeta").attr("class", "valid-card");
+            tarjeta_valida = true;
+
+        } else {
+            $("#type").html("Invalida");
+            $("input").attr("name", "numero_tarjeta").attr("class", "invalid-card");
+            tarjeta_valida = false;
+
+        }
+    }
+    /* !ALGORITMO */
+});
+
+function isValid(input_tarjeta, charCount) {
+    var double = true;
+    var numArr = [];
+    var sumTotal = 0;
+    let i = [0];
+    for (i = 0; i < charCount; i++) {
+        var digito = parseInt(input_tarjeta.charAt(i));
+
+        if (double) {
+            digito = digito * 2;
+            digito = toSingle(digito);
+            double = false;
+        } else {
+            double = true;
+        }
+        numArr.push(digito);
+    }
+
+    for (i = 0; i < numArr.length; i++) {
+        sumTotal += numArr[i];
+    }
+    var diff = eval(sumTotal % 10);
+    console.log(diff);
+    console.log(diff == "0");
+    return (diff == "0");
+}
+
+function toSingle(digito) {
+    if (digito > 9) {
+        var tmp = digito.toString();
+        var d1 = parseInt(tmp.charAt(0));
+        var d2 = parseInt(tmp.charAt(1));
+        return (d1 + d2);
+    } else {
+        return digito;
+    }
+}
+
 // Validación de datos
 let validar = () => {
     let error = false;
@@ -15,7 +91,7 @@ let validar = () => {
     if (input_tarjeta.value == '') {
         error = true;
         input_tarjeta.classList.add('error');
-        console.log('Falta número de tarjeta')
+        console.log('Favor llenar correctamente el campo de número de tarjeta.')
     } else {
         input_tarjeta.classList.remove('error');
     }
@@ -77,11 +153,12 @@ let obtener_datos = () => {
     let vencimiento = input_vencimiento.value;
     let apellido = input_apellido.value;
     let postal = input_postal.value;
+    let tarjeta_valida = true;
 
     btn_registro.addEventListener('click', obtener_datos);
 
     //si hay error, entra al if. Si no hay error entra al else
-    if (validar()) {
+    if (validar() && !tarjeta_valida()) {
         Swal.fire({
             type: 'warning',
             title: '¡Espera!',

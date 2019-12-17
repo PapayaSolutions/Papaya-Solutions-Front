3b0a1da950386dc6c5471d7c0d8173963066f8a3
Wilfredodiazalguera <wdiaza@ucenfotec.ac.cr>
2019-12-17 16:26:55 -0600

    update
    update

--- js/listar_perfiles_servicio.js
+++ js/listar_perfiles_servicio.js
@@ -1,23 +1,23 @@
 'use strct'
 
 let cambiar_estado = async((input_id, input_estado) => {
-            let respuesta = false;
-            await axios({
-                    method: 'post',
-                    url: 'http://localhost:3000/api/cambiar-estado',
-                    responseType: 'json',
-                    //body
-                    data: {
-                        id: input_id,
-                        estado: input_estado
-                    }
-                })
-                .then(function(res) {
-                    console.log(res.data);
-                    respuesta = res;
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-            return respuesta;
-        };
+    let respuesta = false;
+    await axios({
+            method: 'post',
+            url: 'http://localhost:3000/api/modificar-estado',
+            responseType: 'json',
+            //body
+            data: {
+                id: input_id,
+                estado: input_estado
+            }
+        })
+        .then(function(res) {
+            console.log(res.data);
+            respuesta = res;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    return respuesta;
+});
